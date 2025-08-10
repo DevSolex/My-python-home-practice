1ce8c3c9c1c50bfc84e2d63d435ecf4747204fb0
DevSolex <devsolex6@gmail.com>
2025-08-11 00:47:41 +0100

    impliment how to replace just one of two

--- replace.py
+++ replace.py
@@ -1,4 +1,7 @@
-sentence = 'my name is solex and am a student of blockfuse labs cohort III.'
+sentence = 'my name is solex and am a student of blockfuse labs of cohort III.'
 new_sentence = sentence.replace('solex', 'domshak')
+# To replace only just the first 'of'
+replace_of = sentence.replace('of', 'with', 1)
+print(replace_of)
 print(sentence)
 print(new_sentence)
